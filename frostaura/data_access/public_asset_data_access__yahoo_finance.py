e7bdd96deb6ec0293440590eda17313760a0ab97
Dean Martin <dean.martin@derivco.co.za>
2022-08-11 14:08:23 +0200

    [DEV]: Adding of logging to the yahoo finance component and an example for it.

--- frostaura/data_access/public_asset_data_access__yahoo_finance.py
+++ frostaura/data_access/public_asset_data_access__yahoo_finance.py
@@ -1,6 +1,7 @@
 '''This module defines Yahoo Finance data access components.'''
 import yfinance as yf
 import pandas as pd
+from logging import info
 from frostaura.data_access.public_asset_data_access import IPublicAssetDataAccess
 
 class YahooFinanceDataAccess(IPublicAssetDataAccess):
@@ -8,6 +9,8 @@ class YahooFinanceDataAccess(IPublicAssetDataAccess):
 
     def get_symbol_history(self, symbol: str) -> pd.DataFrame:
         '''Get historical price movements for a given symbol.'''
+
+        info(f'Fetching historical price movements for symbol "{symbol}".')
 
         ticker = yf.Ticker(symbol)
         hist = ticker.history(period='max')
