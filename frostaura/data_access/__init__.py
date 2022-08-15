43b7e0efe3992fa391f371e7899503a519a5cccf
Dean Martin <dean.martin@frostaura.net>
2022-08-15 23:04:46 +0200

    [DEV]: Completion of the Telegram data access component with examples.

--- frostaura/data_access/__init__.py
+++ frostaura/data_access/__init__.py
@@ -8,3 +8,6 @@ from frostaura.data_access.public_asset_data_access__yahoo_finance import YahooF
 
 from frostaura.data_access.personal_asset_data_access import IPersonalAssetDataAccess
 from frostaura.data_access.personal_asset_data_access__easy_equities import EasyEquitiesPersonalAssetDataAccess
+
+from frostaura.data_access.notifications_data_access import INotificationsDataAccess
+from frostaura.data_access.notifications_data_access__telegram import TelegramNotificationsDataAccess
