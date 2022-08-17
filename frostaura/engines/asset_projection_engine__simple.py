5f1ccc6da945dc4097509bbf2bc5304a739bc16c
Dean Martin <dean.martin@frostaura.net>
2022-08-17 14:08:21 +0200

    [DEV]: Significantly simplifying the projection engine and updating examples.

--- frostaura/engines/asset_projection_engine__simple.py
+++ frostaura/engines/asset_projection_engine__simple.py
@@ -1,5 +1,6 @@
 '''This module defines projection engine components.'''
 import pandas as pd
+import inflect
 from frostaura.engines.asset_projection_engine import IAssetProjectionEngine
 from frostaura.engines.asset_valuation_engine import IAssetValuationEngine
 from frostaura.models.valuation_result import ValuationResult
@@ -15,6 +16,7 @@ class SimpleAssetProjectionEngine(IAssetProjectionEngine):
         self.asset_valuation_engine = asset_valuation_engine
         self.public_asset_data_access = public_asset_data_access
         self.config = config
+        self.inflect_engine = inflect.engine()
 
     def project_monthly_asset_growth(self,
                                      n_months: int,
@@ -112,5 +114,46 @@ class SimpleAssetProjectionEngine(IAssetProjectionEngine):
             data['total_deposits_withdrawals'].append(sum([p.loc[month_index]['total_deposits_withdrawals'] for p in projections]))
             data['accrued_interest'].append(sum([p.loc[month_index]['accrued_interest'] for p in projections]))
             data['balance'].append(sum([p.loc[month_index]['balance'] for p in projections]))
+
+        return pd.DataFrame(data)
+
+    def project_wealth_growth(self,
+                              holdings_with_profits: pd.DataFrame,
+                              monthly_deposits: list,
+                              n_months: int = 12 * 50, # 50 Years
+                              min_amount: float = 1,
+                              max_amount: float = 100 * 1000 * 10 * 1000 * 10, # Ten Billion
+                              factorials_of: int = 10) -> pd.DataFrame:
+        '''Determine wealth growth at a given annual rates over a specified numbers of months while applying a monthly deposits.'''
+
+        factorials_of: int = 10
+        holdings_growth: pd.DataFrame = self.project_monthly_holdings_growth(n_months=n_months,
+                                                                             holdings_with_profits=holdings_with_profits,
+                                                                             monthly_deposits=monthly_deposits)
+        current_amount_to_project_calculation: float = min_amount
+        data = {
+            'Target Balance (USD)': list(),
+            'Target Balance Eng (USD)': list(),
+            'ETA (Months)': list(),
+            'ETA (Years)': list()
+        }
+        current_balance: float = holdings_with_profits['total_current_usd'].sum()
+
+        while current_amount_to_project_calculation <= max_amount:
+            projections: int = holdings_growth.loc[holdings_growth['balance'] > current_amount_to_project_calculation]
+
+            if current_balance > current_amount_to_project_calculation:
+                current_amount_to_project_calculation *= factorials_of
+                continue
+
+            if projections.shape[0] <= 0:
+                break
+
+            data['Target Balance (USD)'].append(f'{projections.iloc[0]["balance"]:.2f}')
+            data['Target Balance Eng (USD)'].append(self.inflect_engine.number_to_words(projections.iloc[0]["balance"]))
+            data['ETA (Months)'].append(projections.index[0])
+            data['ETA (Years)'].append(projections.index[0] / 12)
+            
+            current_amount_to_project_calculation *= factorials_of
 
         return pd.DataFrame(data)
