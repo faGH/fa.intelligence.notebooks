18289dde5f8d09cb648983190fa3081e5b92e65d
Dean Martin <dean.martin@derivco.co.za>
2022-08-25 16:02:28 +0200

    [DEV]: Adding new method to public asset data access to fetch more symbol data for valuation purposes.

--- frostaura/data_access/public_asset_data_access__yahoo_finance.py
+++ frostaura/data_access/public_asset_data_access__yahoo_finance.py
@@ -4,6 +4,7 @@ import pandas as pd
 from logging import info, warning
 from expiringdict import ExpiringDict
 from frostaura.data_access.public_asset_data_access import IPublicAssetDataAccess
+from frostaura.models.symbol_data import SymbolData
 
 class YahooFinanceDataAccess(IPublicAssetDataAccess):
     '''Yahoo Finance public asset-related functionality.'''
@@ -18,13 +19,40 @@ class YahooFinanceDataAccess(IPublicAssetDataAccess):
 
         info(f'Fetching historical price movements for symbol "{symbol}".')
 
-        value = self.__cache__.get(symbol)
+        cache_key: str = f'{symbol}-history'
+        value: pd.DataFrame = self.__cache__.get(cache_key)
 
-        if value is None:
+        if value is None or ignore_cache:
             warning(f'No item with the key "{symbol}" existed in the cache.')
             ticker = yf.Ticker(symbol)
             value = ticker.history(period='max')
-            self.__cache__[symbol] = value
+            self.__cache__[cache_key] = value
+        else:
+            info(f'Item for key "{symbol}" retrieved from cache with value: {value}')
+
+        return value
+
+    def get_symbol_data(self, symbol: str, ignore_cache: bool = False) -> SymbolData:
+        '''Get detailed information about a given symbol.'''
+
+        info(f'Fetching historical price movements for symbol "{symbol}".')
+
+        cache_key: str = f'{symbol}-data'
+        value: SymbolData = self.__cache__.get(cache_key)
+
+        if value is None or ignore_cache:
+            warning(f'No item with the key "{symbol}" existed in the cache.')
+            ticker = yf.Ticker(symbol)
+            stats = ticker.stats()
+            analysis = ticker.analysis            
+            future_growth_rate: float = analysis.loc['+5Y']['Growth']
+            shares_outstanding: float = stats['defaultKeyStatistics']['sharesOutstanding']
+            free_cash_flow: float = stats['financialData']['freeCashflow']
+            value: SymbolData = SymbolData(free_cash_flow=free_cash_flow,
+                                           future_growth_rate=future_growth_rate,
+                                           shares_outstanding=shares_outstanding,
+                                           symbol=symbol)
+            self.__cache__[cache_key] = value
         else:
             info(f'Item for key "{symbol}" retrieved from cache with value: {value}')
 
--- frostaura/models/__init__.py
+++ frostaura/models/__init__.py
@@ -3,3 +3,4 @@
 from frostaura.models.valuation_result import ValuationResult
 from frostaura.models.profit_calculation_result import ProfitCalculationResult
 from frostaura.models.visualization_types import VisualizationType
+from frostaura.models.symbol_data import SymbolData
